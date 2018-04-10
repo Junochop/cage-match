cc0b4788b62caeea9d954208a14810790e2fdfbe
Juno Lee <lejuno80@gmail.com>
2018-04-09 20:00:33 -0500

    adding challenger

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,69 @@
-console.log("hi");
+
+
+const printToDom = (domString, divId )=>{
+    document.getElementById("winner");
+
+}
+
+
+
+
+
+
+const compareChallengers = () => {
+    const playerName1 = document.getElementById("p1").value;
+    const playerName2 = document.getElementById("p2").value;
+    genericChallengerRequest(executeThisCodeAfterFileLoaded, playerName1);
+}
+
+
+
+
+const pushButton = () => {
+    const buttonSubmit = document.getElementById("submitB");
+    buttonSubmit.addEventListener("click", compareChallengers);
+    
+
+}
+
+
+
+
+
+
+
+
+function executeThisCodeIfXHRFails() {
+    console.log("something broke");
+}
+
+function executeThisCodeAfterFileLoaded() {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+    //buildDomString(data);
+    //addChallengerEventListeners();
+}
+
+const genericChallengerRequest = (successFunction, playerName) => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", successFunction);
+    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+    myRequest.open("GET", `https://teamtreehouse.com/${playerName}.json`);
+    myRequest.send();
+    
+};
+
+
+
+const startApplication = () => {
+    pushButton();
+};
+
+
+startApplication();
+    
+
+
+
+
+
